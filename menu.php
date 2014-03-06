ca52623e5425b482e815fd4b46383bf47fe39d40
Matteo Currò <curromatteo@gmail.com>
2014-03-06 20:14:14 +0100

    hide user link to who doesn't have permission

--- menu.php
+++ menu.php
@@ -1,8 +1,8 @@
 <header>
 	<div class="logo">
 		<a href="index.php">
-			<img class="logo_positivo" src="img/logo_positivo.png" alt="Salco Italia">
-			<img class="logo_negativo" src="img/logo_negativo.png" alt="Salco Italia">
+			<!-- <img class="logo_positivo" src="img/logo_positivo.png" alt="Salco Italia"> -->
+			<img class="logo_positivo" src="img/logo_negativo.png" alt="Salco Italia">
 		</a>
 	</div>
 	<div class="contact">
@@ -13,8 +13,12 @@
 <nav>
 	<ul>
 		<li><a href="index.php">Clienti</a></li>
+		<?php 
+		  if (isset($_SESSION["livello"]) && $_SESSION["livello"] == 1) {
+		?>
 		<li><a href="users.php">Utenti</a></li>
-		<li><a href="form.php">Reminder</a></li>
+		<?php } ?>
+		<li><a href="#">Reminder</a></li>
 		<?php 
 		  if (isset($_SESSION["user_logedIn"]) && $_SESSION["user_logedIn"] === true) {
 		?>
