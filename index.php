fdc81dfa8b8f6a079651eec57c054006e389cc44
Matteo Currò <curromatteo@gmail.com>
2014-03-14 13:16:56 +0100

    Remove content moved to clients.php and add a welcome message

--- index.php
+++ index.php
@@ -10,70 +10,17 @@ require("header.php");
 
 ?>
 	<div class="wrapper">
-	<h2>Clienti</h2>
-	<a class="button float_r" href="add_client.php">Aggiungi nuovo</a>
-	<table class="clients">
-		<tr>
-		<th>ID</th>
-		<th>Nome</th>
-		<th>Cognome</th>
-		<th>Citt&agrave;</th>
-		<th>Data</th>
-		<th></th>
-		<th></th>
-		<th></th>
-		</tr>
+	<?php
+  // effettuo una query per recuperare i dati relativi al cliente con l'id passato in get
+    $utenti = query('SELECT * FROM utenti where id = :id LIMIT 1', array('id' => $_SESSION["user_id"]), $conn);
+    if ($utenti) {
+      // ciclo il cliente
+      foreach ($utenti as $utente) { ?>
 
-<?php
-// effettuo una query per recuperare i dati relativi ai clienti
-	$utenti = query('SELECT * FROM clienti ORDER BY id DESC LIMIT 100', array(), $conn);
-	if ($utenti) {
-		// ciclo il cliente
-		foreach ($utenti as $utente) {
-			// print_r($utente);
-			?>
-		<tr>
-			<td><?php echo $utente['id']; ?></td>
-			<td><?php echo $utente['nome']; ?></td>
-			<td><?php echo $utente['cognome']; ?></td>
-			<td><?php echo $utente['citta']; ?></td>
-			<td><?php 
-					$data = strtotime($utente['data_incontro']);
-					echo date('d/m/y',$data); 
-				?></td>
-			<td><a class="button" href="view_client.php?id=<?php echo $utente['id']; ?>">View</a></td>
-			<td><a class="button" href="edit_client.php?id=<?php echo $utente['id']; ?>">Edit</a></td>
-			<td><a class="button red delete" href="delete_client.php?id=<?php echo $utente['id']; ?>">Delete</a></td>
-		</tr>
-		<?php }
-	} ?>
-	</table>
-	</div>
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
-<script>
-// azioni all'invio del form
-$('.delete').on('click', function (e) {
-  e.preventDefault();
-  var $this = $(this);
-  // conferma
-    if (confirm("Sei sicuro di voler eliminare l'elemento selezionato?")) {
-	  // effettuo la chiamata ajax
-	  $.ajax({
-	      type: 'GET',
-	      url: $this.attr('href'),
-	      cache: false,
-	      // traditional: true,
-	      data: {
-	        // commissioni: array_commissioni
-	      },
-	      success: function(data) {
-	        $this.parent().parent('tr').fadeOut(400);
-	         return false;
-	      }
-	  });
-     }     
-});
-</script>
+	<h2>Benvenuto <?php echo $utente['nome']; ?>!</h2>
+	<p>Utilizza il men&ugrave; per navigare tra le pagine.</p>
+	<?php  } // Fine foreach
+    } // Fine if utenti ?>
 
 <?php
 require('footer.php');
